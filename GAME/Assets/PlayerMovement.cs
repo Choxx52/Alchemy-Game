8a2cf1c021fffcd4dce7d89937db8468f1a4499a
Choxx52 <demidusvelikolepniy005@gmail.com>
2025-04-18 00:02:32 +0300

    Объект с передвижением

--- GAME/Assets/PlayerMovement.cs
+++ GAME/Assets/PlayerMovement.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
     void Start()
     {
-        
+        targetPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            targetPosition = mousePos;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            
+            if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+            {
+                isMoving = false;
+            }
+        }
     }
 }
